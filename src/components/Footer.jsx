25f79a6b6250ab1c6ccca704c18921b6f1e30552
souperwit <sirawit.mn@gmail.com>
2021-03-22 00:00:36 +0700

    Revert "keeper app part1"
    This reverts commit 8e431b87e242995b431a9153fc0c4a0903cc6343.

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const getYear = new Date().getFullYear();
-
-function Footer() {
-  return <footer><p>Copyright © {getYear} Sirawit Mahanin</p></footer>; 
-}
-
-export default Footer;
