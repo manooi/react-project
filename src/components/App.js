0f32b3b350b38933153389daf3738895be68fe69
souperwit <sirawit.mn@gmail.com>
2021-03-23 00:26:56 +0700

    add map function

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,24 @@ import React from 'react';
 import Card from './Card';
 import contacts from '../contacts'
 
+
+function createCard(contact) {
+  return <Card
+    key={contact.id}
+    id={contact.id}
+    name={contact.name}
+    imgUrl={contact.imgURL}
+    email={contact.email}
+    phone={contact.phone}
+  />;
+}
+
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
-        name={contacts[0].name}
-        email={contacts[0].email}
-        imgUrl={contacts[0].imgURL}
-        phone={contacts[0].phone} />
-      <Card
-        name={contacts[1].name}
-        email={contacts[1].email}
-        imgUrl={contacts[1].imgURL}
-        phone={contacts[1].phone} />
-      <Card
-        name={contacts[2].name}
-        email={contacts[2].email}
-        imgUrl={contacts[2].imgURL}
-        phone={contacts[2].phone} />
+      {contacts.map(createCard)}
     </div>
   );
 }
