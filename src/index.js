789a3fca43bd3c5496b60f446d8151e534c8db66
souperwit <sirawit.mn@gmail.com>
2021-03-23 22:34:27 +0700

    reduce function

--- src/index.js
+++ src/index.js
@@ -6,33 +6,10 @@ ReactDOM.render(
     <App />, document.getElementById('root')
 );
 
-
 var numbers = [3, 56, 2, 48, 1];
 
+var sum = numbers.reduce(function (acc, num) {
+    return acc + num;
+})
 
-function myFilter(x) {
-    if (x > 5) {
-        return true
-    } else {
-        return false
-    }
-}
-
-var filtered = numbers.filter(myFilter);
-
-var filtered2 = numbers.filter(function (x) {
-    return x > 2;
-});
-
-var filtered3 = numbers.filter(x => { return x < 50 });
-
-console.log(filtered);
-console.log(filtered2);
-console.log(filtered3);
-
-
-
-
-
-
-
+console.log(sum);
