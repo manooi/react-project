471a1c01819a3e75631c078ef38142da0c591123
souperwit <sirawit.mn@gmail.com>
2021-04-06 20:22:51 +0700

    Complex state

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,41 @@
 import React, { useState } from "react";
 
 function App() {
-
-  const [name, setName] = useState("")
-  const [headingText, setHeadingText] = useState("")
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
 
   function handleChange(event) {
-    setName(event.target.value);
-    // console.log(event.target.value);
-    // setName(event.target.placeholder);
-  }
+    const { value, name } = event.target
+    // const inputValue = event.target.value
+    // const inputName = event.target.name
 
-  function handleClick(event) {
-    setHeadingText(name)
-
-    // ห้ามไม่ให้ Refresh!
-    event.preventDefault();
+    setContact((prev) => {
+      // ห้ามใช้ event.target.name หรืออื่น ๆ ใน setter นี้
+      // จะเกิด synthetic event error
+      if (name === "fName") {
+        return { fName: value, lName: prev.lName, email: prev.email };
+      } else if (name === "lName") {
+        return { lName: value, fName: prev.fName, email: prev.email };
+      } else {
+        return { email: value, fName: prev.fName, lName: prev.lName };
+      }
+    });
   }
 
   return (
     <div className="container">
-      <h1>Hello {headingText}</h1>
-      <form onSubmit={handleClick}>
-        <input onChange={handleChange} type="text" placeholder="What's your name?" value={name} />
-        <button type="submit">Submit </button>
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input onChange={handleChange} name="fName" placeholder="First Name" value={contact.fName} />
+        <input onChange={handleChange} name="lName" placeholder="Last Name" value={contact.lName} />
+        <input onChange={handleChange} name="email" placeholder="Email" value={contact.email} />
+        <button>Submit</button>
       </form>
     </div>
   );
