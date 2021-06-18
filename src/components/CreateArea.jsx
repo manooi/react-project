8c569aaff7afafd3da5f01f4660d9dfeb4b75b98
souperwit <sirawit.mn@gmail.com>
2021-06-18 11:50:00 +0700

    Keeper app finished

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,7 +1,19 @@
 import React, { useState } from "react";
 import AddCircleIcon from '@material-ui/icons/AddCircle';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
+
+
+  const [isClick, setClick] = useState(false);
+
+
+  function handleClick() {
+    setClick(true);
+  }
+
+
   const [note, setNote] = useState({
     title: "",
     content: ""
@@ -30,22 +42,29 @@ function CreateArea(props) {
   return (
     <div>
       <form className="create-note">
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
+
+        {isClick &&
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />}
+
         <textarea
           name="content"
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={isClick ? "3" : "1"}
+          onClick={handleClick}
         />
-        <button onClick={submitNote}><AddCircleIcon /></button>
+        <Zoom in={isClick} timeout={500}>
+          <Fab onClick={submitNote}><AddCircleIcon /></Fab>
+        </Zoom>
+
       </form>
-    </div>
+    </div >
   );
 }
 
